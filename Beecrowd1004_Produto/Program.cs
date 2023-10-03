879756bb9423b0b52392563adb07db101065f353
PedroMoraes <pedroomoraes@gmail.com>
2023-10-03 19:50:53 -0300

    Programa pronto e aceito peloi Beecrowd

--- Beecrowd1004_Produto/Program.cs
+++ Beecrowd1004_Produto/Program.cs
@@ -4,7 +4,14 @@ namespace Beecrowd {
     class Program1004 {
         static void Main(string[] args) {
 
+            int ValorA, ValorB, Produto;
 
+            ValorA = int.Parse(Console.ReadLine());
+            ValorB = int.Parse(Console.ReadLine());
+
+            Produto = ValorA * ValorB;
+
+            Console.WriteLine("PROD = " + Produto);
         }
     }
 }
